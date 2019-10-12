611d368a8cc94442119ee3e32ffc98517f6b6fe1
Pascal Kaufmann <pasu@Pascals-MacBook-Pro.local>
2019-10-12 11:44:10 +0900

    update project desc

--- main/main.ino
+++ main/main.ino
@@ -1,31 +1,17 @@
 /*
   Short name or description of project
   Long description of the project. What does this project do and why was is made for example.
+  
   Pinout
   | Arduino | Some other hardware |
   |---------|---------------------|
   | D4      | ENA                 |
-  | D5      | IN1                 |
-  | D6      | IN2                 |
-  | Vcc     | Vcc                 |
-  | GND     | GND                 |
-  Created 01 January 2016
-  By Bram Harmsen
-  https:/github.com/bremme/my-repro
+  
+  Created 12 October 2019
+  https://github.com/pasu-ka/inter_des
   Change log
   * version 0.1
     + initial release
-  License
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 // include libraries
