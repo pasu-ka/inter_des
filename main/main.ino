1dbc3ab3d0e4cbcd17d15700492b77634e3c07f8
Pascal Kaufmann <pasu@Pascals-MacBook-Pro.local>
2019-11-01 15:05:25 +0900

    leaf wiggle func

--- main/main.ino
+++ main/main.ino
@@ -1,30 +1,50 @@
 /*
   Short name or description of project
   Long description of the project. What does this project do and why was is made for example.
-  
+
   Pinout
-  | Arduino | Some other hardware |
+  | Arduino | Hardware            |
   |---------|---------------------|
-  | D4      | ENA                 |
-  
+  | D3      | Leaf Servo          |
+
   Created 12 October 2019
   https://github.com/pasu-ka/inter_des
-  Change log
-  * version 0.1
-    + initial release
+
 */
 
 // include libraries
+#include <Servo.h>
+
 
 // initialize globals
+int servoPin = 3;
+Servo leafServo;
+int leafWiggleAngleAmount[] = {120, 60, 100, 0};
+
 
 // run setup code
 void setup() {
-
-};
+  setupLeafServo();
+}
 
 
 // run loop (forever)
 void loop() {
 
-};
+}
+
+
+void setupLeafServo() {
+  leafServo.attach(servoPin);
+  leafServo.write(0);
+}
+
+
+void wiggleLeaf(int wiggleRepetition) {
+  for(int i = 0; i < wiggleRepetition; i++) {
+    for(int wiggleAngle : leafWiggleAngleAmount) {
+      leafServo.write(wiggleAngle);
+      delay(300);
+    }
+  }
+}
