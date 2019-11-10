a632ea280acf008333bac3f236a0977cff319fd4
Pascal Kaufmann <pasu@Pascals-MacBook-Pro.local>
2019-11-10 11:14:13 +0900

    messy introduction of pt thred

--- main/main.ino
+++ main/main.ino
@@ -15,6 +15,14 @@
 
 // include libraries
 #include <Servo.h>
+// #include <clock-arch.h>
+// #include <lc.h>
+// #include <lc-addrlabels.h>
+// #include <pt-sem.h>
+#include <timer.h>
+// #include <clock.h>
+// #include <lc-switch.h>
+#include <pt.h> // threading
 
 
 // initialize globals
@@ -22,42 +30,114 @@ Servo leafServo;
 
 int servoPin = 3;
 int micPinAnalogue = A0;
+
 int leafWiggleAngleAmount[] = {40, 180, 0};
-int soundLevelThreshold = 715;
+int soundLevelThreshold = 80;
 enum states {
-    sleep,
-    awake,
-    happy,
-    scared,
-    listening
+  sleep,
+  awake,
+  happy,
+  scared,
+  listening
 };
 int currentState;
 
 bool debugMode = false;
 
+static int TIMEOUT = 5000;
+struct timer timer;
+
+
+// every protothread needs an own struct pt variable
+static struct pt pt1, pt2, pt3;
+
+// function declaration, because C is...
+
+bool soundTresholdReached();
+void logDebug(String text, int val);
+void logDebug(String text);
 
 // run setup code
-void setup() {
-  currentState = sleep;
-  startDebug();
+ void setup() {
+ currentState = sleep;
+
+ startDebug();
   setupLeafServo();
-}
+  /* Initialize the protothread state variables with PT_INIT(). */
+  PT_INIT(&pt1);
+  PT_INIT(&pt2);
+  PT_INIT(&pt3);
+  /* Then we schedule the two protothreads by repeatedly calling their
+    protothread functions and passing a pointer to the protothread
+    state variables as arguments.     */
+  // while(1) {
+ }
 
 
 // run loop (forever)
-void loop() {
-  if(debugMode) {
-    microPhoneTest();
-    if(currentState == awake) {
-      wiggleLeaf(1);
-    }
+ void loop() {
+//   if(debugMode) {
+//     microPhoneTest();
+//     if(currentState == awake) {
+//       wiggleLeaf(1);
+//     }
+//   }
+//   checkState();
+  listenThread(&pt1);
+  timeoutThread(&pt2);
+//  moveThread(&pt3);
+
+ }
+
+static int listenThread(struct pt *pt) {
+  PT_BEGIN(pt);
+  while (1) {
+    PT_WAIT_UNTIL(pt, soundTresholdReached());
+    logDebug("waking up");
+    wakeUp();
   }
-  checkState();
+  PT_END(pt);
 }
 
+static int timeoutThread(struct pt *pt) {
+  PT_BEGIN(pt);
+  do {
+    timer_set(&timer, TIMEOUT);
+    PT_WAIT_UNTIL(pt, timer_expired(&timer));
+    if (currentState == awake) {
+      currentState = sleep;
+     logDebug("going to sleep from awake");
+//      sleepNow();
+    } else if (currentState == listening) {
+      currentState = listening;
+     logDebug("going to awake from listening");
+//      goAwake();
+    }
+
+    // PT_WAIT_UNTIL(pt, timeoutReached());
+    // do_something();
+  } while (1);
+  PT_END(pt); // TODO not necessary?
+}
+
+static void resetTimer() {
+ logDebug("reset timer");
+ // PT_END(&pt2);
+ // timeoutThread(&pt2);
+}
+
+static int moveThread(struct pt *pt) {
+  PT_BEGIN(pt);
+  while (1) {
+    // TODO move code here
+    // PT_WAIT_UNTIL(pt, function_returns_true());
+    // do_something();
+  }
+  PT_END(pt);
+}
 
 void checkState() {
-  switch(currentState) {
+  switch (currentState) {
     case sleep:
       checkSleepState();
       break;
@@ -74,10 +154,13 @@ void checkState() {
   // TODO implement
 }
 
+void timeoutReached() {
+  return;
+}
 
 void checkSleepState() {
   //TODO pickup || noise
-  if(soundTresholdReached()) {
+  if (soundTresholdReached()) {
     wakeUp();
   }
 }
@@ -85,7 +168,7 @@ void checkSleepState() {
 
 void checkAwakeState() {
   //TODO do right
-  if(!soundTresholdReached()) {
+  if (!soundTresholdReached()) {
     sleepNow();
   }
 }
@@ -93,11 +176,19 @@ void checkAwakeState() {
 
 void sleepNow() {
   currentState = sleep;
+  resetTimer();
 }
 
 
 void wakeUp() {
   currentState = awake;
+  wiggleLeaf(1);
+//  resetTimer();
+}
+
+void goAwake() {
+  currentState = awake;
+  resetTimer();
 }
 
 
@@ -107,6 +198,7 @@ bool soundTresholdReached() {
 }
 
 int readSoundLevel() {
+  int lvl = analogRead(micPinAnalogue);
   return analogRead(micPinAnalogue);
 }
 
@@ -116,10 +208,10 @@ void setupLeafServo() {
   leafServo.write(0);
 }
 
-
+// TODO make protothread
 void wiggleLeaf(int wiggleRepetition) {
-  for(int i = 0; i < wiggleRepetition; i++) {
-    for(int wiggleAngle : leafWiggleAngleAmount) {
+  for (int i = 0; i < wiggleRepetition; i++) {
+    for (int wiggleAngle : leafWiggleAngleAmount) {
       leafServo.write(wiggleAngle);
       delay(150);
     }
@@ -127,14 +219,14 @@ void wiggleLeaf(int wiggleRepetition) {
 }
 
 
-void startDebug() {
+static void startDebug() {
   debugMode = true;
   Serial.begin(9600);
 }
 
 
 void microPhoneTest() {
-  if (readSoundLevel() > soundLevelThreshold){
+  if (readSoundLevel() > soundLevelThreshold) {
     logDebug("HIGH");
   } else {
     logDebug("LOW");
@@ -142,6 +234,35 @@ void microPhoneTest() {
 }
 
 
-void logDebug(String text) {
-  Serial.println(text);
+static void logDebug(String text) {
+  if (debugMode) {
+    Serial.println("### debug ###");
+    Serial.println(text);
+  }
 }
+
+static void logDebug(String text, int val) {
+  if (debugMode) {
+    Serial.println("### debug ###");
+    Serial.print(text);
+    Serial.println(val);
+  }
+}
+
+//int main(void) {
+//  setup();
+//  startDebug();
+//  setupLeafServo();
+//  /* Initialize the protothread state variables with PT_INIT(). */
+//  PT_INIT(&pt1);
+//  PT_INIT(&pt2);
+//  PT_INIT(&pt3);
+//  /* Then we schedule the two protothreads by repeatedly calling their
+//    protothread functions and passing a pointer to the protothread
+//    state variables as arguments.     */
+//  // while(1) {
+//  listenThread(&pt1);
+//  timeoutThread(&pt2);
+//  moveThread(&pt3);
+//  // }
+//}
