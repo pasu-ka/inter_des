56cbf0fef1dced6780a892208b2c8f33b2f08fe7
Pascal Kaufmann <pasu@Pascals-MacBook-Pro.local>
2019-11-15 13:04:29 +0900

    initial eyes code

--- main/main.ino
+++ main/main.ino
@@ -23,6 +23,10 @@
 
 // include libraries
 
+#include <Adafruit_LEDBackpack.h>
+#include <Adafruit_GFX.h>
+#include <Arduino.h>
+#include <Wire.h>
 #include <Servo.h>
 // #include <clock-arch.h>
 // #include <lc.h>
@@ -72,8 +76,6 @@ enum soundLevel {
 
 
 // configurations
-
-
 float soundLevelThresholdWakeup     = 1.45;
 float soundLevelThresholdScared     = 2.43;
 int leafWiggleAngleAmount[]   = {40, 180, 0};
@@ -85,6 +87,141 @@ const static int STATE_CHANGE_TIMEOUT = 5000;
 const int VIBRA_STRONG                = 250;
 const int VIBRA_MIDDLE                = 160;
 const int VIBRA_LOW                   = 80;
+
+
+// LED matrix vars
+#define MATRIX_EYES         0
+#define MATRIX_MOUTH_LEFT   1
+#define MATRIX_MOUTH_MIDDLE 2
+#define MATRIX_MOUTH_RIGHT  3
+Adafruit_8x8matrix matrix[4] = { // Array of Adafruit_8x8matrix objects
+  Adafruit_8x8matrix(), Adafruit_8x8matrix(),
+  Adafruit_8x8matrix(), Adafruit_8x8matrix()
+};
+
+// Rather than assigning matrix addresses sequentially in a loop, each
+// has a spot in this array.  This makes it easier if you inadvertently
+// install one or more matrices in the wrong physical position --
+// re-order the addresses in this table and you can still refer to
+// matrices by index above, no other code or wiring needs to change.
+static const uint8_t matrixAddr[] = { 0x70, 0x71, 0x72, 0x73 };
+
+static const uint8_t PROGMEM // Bitmaps are stored in program memory
+blinkImg[][8] = {    // Eye animation frames
+  { B00111100,         // Fully open eye
+    B01111110,
+    B11111111,
+    B11111111,
+    B11111111,
+    B11111111,
+    B01111110,
+    B00111100
+  },
+  { B00000000,
+    B01111110,
+    B11111111,
+    B11111111,
+    B11111111,
+    B11111111,
+    B01111110,
+    B00111100
+  },
+  { B00000000,
+    B00000000,
+    B00111100,
+    B11111111,
+    B11111111,
+    B11111111,
+    B00111100,
+    B00000000
+  },
+  { B00000000,
+    B00000000,
+    B00000000,
+    B00111100,
+    B11111111,
+    B01111110,
+    B00011000,
+    B00000000
+  },
+  { B00000000,         // Fully closed eye
+    B00000000,
+    B00000000,
+    B00000000,
+    B10000001,
+    B01111110,
+    B00000000,
+    B00000000
+  }
+},
+mouthImg[][24] = {                 // Mouth animation frames
+  { B00000000, B00000000, B00000000, // Mouth position A
+    B00000000, B00000000, B00000000,
+    B01111111, B11111111, B11111110,
+    B00000000, B00000000, B00000000,
+    B00000000, B00000000, B00000000,
+    B00000000, B00000000, B00000000,
+    B00000000, B00000000, B00000000,
+    B00000000, B00000000, B00000000
+  },
+  { B00000000, B00000000, B00000000, // Mouth position B
+    B00000000, B00000000, B00000000,
+    B00111111, B11111111, B11111100,
+    B00000111, B00000000, B11100000,
+    B00000000, B11111111, B00000000,
+    B00000000, B00000000, B00000000,
+    B00000000, B00000000, B00000000,
+    B00000000, B00000000, B00000000
+  },
+  { B00000000, B00000000, B00000000, // Mouth position C
+    B00000000, B00000000, B00000000,
+    B00111111, B11111111, B11111100,
+    B00001000, B00000000, B00010000,
+    B00000110, B00000000, B01100000,
+    B00000001, B11000011, B10000000,
+    B00000000, B00111100, B00000000,
+    B00000000, B00000000, B00000000
+  },
+  { B00000000, B00000000, B00000000, // Mouth position D
+    B00000000, B00000000, B00000000,
+    B00111111, B11111111, B11111100,
+    B00100000, B00000000, B00000100,
+    B00010000, B00000000, B00001000,
+    B00001100, B00000000, B00110000,
+    B00000011, B10000001, B11000000,
+    B00000000, B01111110, B00000000
+  },
+  { B00000000, B00000000, B00000000, // Mouth position E
+    B00000000, B00111100, B00000000,
+    B00011111, B11000011, B11111000,
+    B00000011, B10000001, B11000000,
+    B00000000, B01111110, B00000000,
+    B00000000, B00000000, B00000000,
+    B00000000, B00000000, B00000000,
+    B00000000, B00000000, B00000000
+  },
+  { B00000000, B00111100, B00000000, // Mouth position F
+    B00000000, B11000011, B00000000,
+    B00001111, B00000000, B11110000,
+    B00000001, B00000000, B10000000,
+    B00000000, B11000011, B00000000,
+    B00000000, B00111100, B00000000,
+    B00000000, B00000000, B00000000,
+    B00000000, B00000000, B00000000
+  }
+};
+
+uint8_t
+blinkIndex[] = { 1, 2, 3, 4, 3, 2, 1 }, // Blink bitmap sequence
+               blinkCountdown = 100, // Countdown to next blink (in frames)
+               gazeCountdown  =  75, // Countdown to next eye movement
+               gazeFrames     =  50, // Duration of eye movement (smaller = faster)
+               mouthPos       =   0, // Current image number for mouth
+               mouthCountdown =  10; // Countdown to next mouth change
+int8_t
+eyeX = 3, eyeY = 3,   // Current eye position
+newX = 3, newY = 3,   // Next eye position
+dX   = 0, dY   = 0;   // Distance from prior to new position
 
 
 // function declaration, because dark side of C
@@ -101,6 +238,8 @@ void goAwakeFromListening();
 void goHappy();
 void goScared();
 void goListening();
+void setupEyeLedMatrix();
+void playWithEyesDebug();
 
 
 // run setup code
@@ -113,6 +252,7 @@ void setup() {
   currentState = sleep;
 
   setupLeafServo();
+  setupEyeLedMatrix();
   /* Initialize the protothread state variables with PT_INIT(). */
   PT_INIT(&pt1);
   PT_INIT(&pt2);
@@ -122,6 +262,9 @@ void setup() {
 
 // run loop (forever)
 void loop() {
+  if (debugMode) {
+    playWithEyesDebug();
+  }
   listenThread(&pt1);
   timeoutThread(&pt2);
   //  moveThread(&pt3);
@@ -265,7 +408,7 @@ int soundThresholdReached() {
   unsigned int signalMax = 0;
   unsigned int signalMin = 1024;
 
-  // collect data for 50 mS
+  // collect data for x mS
   while (millis() - startMillis < SOUND_SAMPLE_WINDOW)
   {
     soundSample = analogRead(0);
@@ -305,6 +448,85 @@ void setupLeafServo() {
   leafServo.write(0);
 }
 
+void setupEyeLedMatrix() {
+  // Seed random number generator from an unused analog input:
+  randomSeed(analogRead(A1));
+
+  // Initialize each matrix object:
+  for (uint8_t i = 0; i < 4; i++) {
+    matrix[i].begin(matrixAddr[i]);
+    // If using 'small' (1.2") displays vs. 'mini' (0.8"), enable this:
+    // matrix[i].setRotation(3);
+  }
+}
+
+// Draw mouth image across three adjacent displays
+void drawMouth(const uint8_t *img) {
+  for (uint8_t i = 0; i < 3; i++) {
+    matrix[MATRIX_MOUTH_LEFT + i].clear();
+    matrix[MATRIX_MOUTH_LEFT + i].drawBitmap(i * -8, 0, img, 24, 8, LED_ON);
+  }
+}
+
+void playWithEyesDebug() {
+  // Draw eyeball in current state of blinkyness (no pupil).  Note that
+  // only one eye needs to be drawn.  Because the two eye matrices share
+  // the same address, the same data will be received by both.
+  matrix[MATRIX_EYES].clear();
+  // When counting down to the next blink, show the eye in the fully-
+  // open state.  On the last few counts (during the blink), look up
+  // the corresponding bitmap index.
+  matrix[MATRIX_EYES].drawBitmap(0, 0,
+                                 blinkImg[
+                                   (blinkCountdown < sizeof(blinkIndex)) ? // Currently blinking?
+                                   blinkIndex[blinkCountdown] :            // Yes, look up bitmap #
+                                   0                                       // No, show bitmap 0
+                                 ], 8, 8, LED_ON);
+  // Decrement blink counter.  At end, set random time for next blink.
+  if (--blinkCountdown == 0) blinkCountdown = random(5, 180);
+
+  // Add a pupil (2x2 black square) atop the blinky eyeball bitmap.
+  // Periodically, the pupil moves to a new position...
+  if (--gazeCountdown <= gazeFrames) {
+    // Eyes are in motion - draw pupil at interim position
+    matrix[MATRIX_EYES].fillRect(
+      newX - (dX * gazeCountdown / gazeFrames),
+      newY - (dY * gazeCountdown / gazeFrames),
+      2, 2, LED_OFF);
+    if (gazeCountdown == 0) {   // Last frame?
+      eyeX = newX; eyeY = newY; // Yes.  What's new is old, then...
+      do { // Pick random positions until one is within the eye circle
+        newX = random(7); newY = random(7);
+        dX   = newX - 3;  dY   = newY - 3;
+      } while ((dX * dX + dY * dY) >= 10);     // Thank you Pythagoras
+      dX            = newX - eyeX;             // Horizontal distance to move
+      dY            = newY - eyeY;             // Vertical distance to move
+      gazeFrames    = random(3, 15);           // Duration of eye movement
+      gazeCountdown = random(gazeFrames, 120); // Count to end of next movement
+    }
+  } else {
+    // Not in motion yet -- draw pupil at current static position
+    matrix[MATRIX_EYES].fillRect(eyeX, eyeY, 2, 2, LED_OFF);
+  }
+
+  // Draw mouth, switch to new random image periodically
+  drawMouth(mouthImg[mouthPos]);
+  if (--mouthCountdown == 0) {
+    mouthPos = random(6); // Random image
+    // If the 'neutral' position was chosen, there's a 1-in-5 chance we'll
+    // select a longer hold time.  This gives the appearance of periodic
+    // pauses in speech (e.g. between sentences, etc.).
+    mouthCountdown = ((mouthPos == 0) && (random(5) == 0)) ?
+                     random(10, 40) : // Longer random duration
+                     random(2, 8);    // Shorter random duration
+  }
+
+  // Refresh all of the matrices in one quick pass
+  for (uint8_t i = 0; i < 4; i++) matrix[i].writeDisplay();
+
+  delay(20); // ~50 FPS
+}
+
 
 // TODO make protothread
 void wiggleLeaf(int wiggleRepetition) {
@@ -320,15 +542,6 @@ void wiggleLeaf(int wiggleRepetition) {
 static void startDebug() {
   Serial.begin(9600);
   logDebug("starting debug");
-}
-
-
-void microPhoneTest() {
-  if (readSoundLevel() > soundLevelThresholdWakeup) {
-    logDebug("HIGH");
-  } else {
-    logDebug("LOW");
-  }
 }
 
 
